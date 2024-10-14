6886d33469af330bff066037ce7fd09d940e81fb
dlord213 <lollingworld41@gmail.com>
2024-10-14 20:15:18 +0800

    schedules: created a custom hook & added modal for adding schedule (functions for storing to be implemented) & other changes

--- hooks/useSchedule.tsx
+++ hooks/useSchedule.tsx
@@ -75,7 +75,7 @@ export default function useSchedule() {
 
     setScheduleForm((prevSched: any) => ({
       ...prevSched,
-      enDate: formattedDate,
+      endDate: formattedDate,
     }));
   };
 
