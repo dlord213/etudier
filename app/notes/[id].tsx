78c4f2027f9a96b23bb42d754d4834727c275faa
dlord213 <lollingworld41@gmail.com>
2024-10-31 15:34:56 +0800

    notes: added loading state

--- app/notes/[id].tsx
+++ app/notes/[id].tsx
@@ -1,6 +1,12 @@
 import { useLocalSearchParams } from "expo-router";
 import React, { useCallback, useEffect, useState } from "react";
-import { StyleSheet, TextInput, useWindowDimensions, View } from "react-native";
+import {
+  ActivityIndicator,
+  StyleSheet,
+  TextInput,
+  useWindowDimensions,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { router, useFocusEffect, useNavigation } from "expo-router";
 
@@ -22,6 +28,8 @@ export default function Page() {
   const { findNote, setTitle, setDescription, form, saveNoteChanges } =
     useNoteStore();
 
+  const [loaded, setLoaded] = useState(false);
+
   const styleState = styles(isDarkMode, isOLEDMode);
   const navigation = useNavigation();
 
@@ -34,8 +42,11 @@ export default function Page() {
   useFocusEffect(
     useCallback(() => {
       findNote(id);
+      setTimeout(() => {
+        setLoaded(true);
+      }, 250);
 
-      const handleBeforeRemove = (e) => {
+      const handleBeforeRemove = (e: any) => {
         e.preventDefault();
 
         saveNoteChanges();
@@ -56,62 +67,84 @@ export default function Page() {
   }, [form.description]);
 
   return (
-    <SafeAreaView style={styleState.safeAreaView}>
-      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-        <AntDesign
-          name="close"
-          size={24}
-          color={iconColor}
-          onPress={() => router.back()}
-        />
-        <MaterialCommunityIcons
-          name="dots-vertical"
-          size={24}
-          color={iconColor}
-        />
-      </View>
-      <TextInput
-        style={{
-          fontFamily: "WorkSans_900Black",
-          fontSize: 24,
-          color: isDarkMode
-            ? Colors.Text_Dark.Default
-            : Colors.Text_Light.Default,
-        }}
-        placeholderTextColor={
-          isDarkMode ? Colors.Text_Dark.Secondary : Colors.Text_Light.Secondary
-        }
-        cursorColor={Colors[palette][600]}
-        selectionColor={Colors[palette][600]}
-        selectionHandleColor={Colors[palette][600]}
-        value={form.title}
-        onChangeText={setTitle}
-        placeholder="Note title"
-      />
-      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-        <ThemedText text={startOfToday().toDateString()} color="Secondary" />
-        <ThemedText text={charCount.toString()} color="Secondary" />
-      </View>
-      <TextInput
-        style={{
-          fontFamily: "WorkSans_400Regular",
-          color: isDarkMode
-            ? Colors.Text_Dark.Default
-            : Colors.Text_Light.Default,
-          minHeight: screenHeight / 2,
-          textAlignVertical: "top",
-        }}
-        multiline
-        placeholderTextColor={
-          isDarkMode ? Colors.Text_Dark.Secondary : Colors.Text_Light.Secondary
-        }
-        cursorColor={Colors[palette][600]}
-        selectionColor={Colors[palette][600]}
-        selectionHandleColor={Colors[palette][600]}
-        value={form.description}
-        onChangeText={setDescription}
-        placeholder="Jot down your thoughts, ideas, or reminders here..."
-      />
+    <SafeAreaView
+      style={[
+        styleState.safeAreaView,
+        !loaded ? { justifyContent: "center", alignItems: "center" } : null,
+      ]}
+    >
+      {loaded ? (
+        <>
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            <AntDesign
+              name="close"
+              size={24}
+              color={iconColor}
+              onPress={() => router.back()}
+            />
+            <MaterialCommunityIcons
+              name="dots-vertical"
+              size={24}
+              color={iconColor}
+            />
+          </View>
+          <TextInput
+            style={{
+              fontFamily: "WorkSans_900Black",
+              fontSize: 24,
+              color: isDarkMode
+                ? Colors.Text_Dark.Default
+                : Colors.Text_Light.Default,
+            }}
+            placeholderTextColor={
+              isDarkMode
+                ? Colors.Text_Dark.Secondary
+                : Colors.Text_Light.Secondary
+            }
+            cursorColor={Colors[palette][600]}
+            selectionColor={Colors[palette][600]}
+            selectionHandleColor={Colors[palette][600]}
+            value={form.title}
+            onChangeText={setTitle}
+            placeholder="Note title"
+          />
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            <ThemedText
+              text={startOfToday().toDateString()}
+              color="Secondary"
+            />
+            <ThemedText text={charCount.toString()} color="Secondary" />
+          </View>
+          <TextInput
+            style={{
+              fontFamily: "WorkSans_400Regular",
+              color: isDarkMode
+                ? Colors.Text_Dark.Default
+                : Colors.Text_Light.Default,
+              minHeight: screenHeight / 2,
+              textAlignVertical: "top",
+            }}
+            multiline
+            placeholderTextColor={
+              isDarkMode
+                ? Colors.Text_Dark.Secondary
+                : Colors.Text_Light.Secondary
+            }
+            cursorColor={Colors[palette][600]}
+            selectionColor={Colors[palette][600]}
+            selectionHandleColor={Colors[palette][600]}
+            value={form.description}
+            onChangeText={setDescription}
+            placeholder="Jot down your thoughts, ideas, or reminders here..."
+          />
+        </>
+      ) : (
+        <ActivityIndicator size={64} color={Colors[palette][400]} />
+      )}
     </SafeAreaView>
   );
 }
